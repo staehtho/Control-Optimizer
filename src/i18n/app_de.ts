dd9579ca4ef0a44ebabf20a60684f63ac855938e
Thomas Stähli <staehtho@students.zhaw.ch>
2026-02-22 20:43:30 +0100

    update .ts files

--- src/i18n/app_de.ts
+++ src/i18n/app_de.ts
@@ -1,4 +1,152 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
+<context>
+    <name>FunctionModel</name>
+    <message>
+        <location filename="../models/functionModel.py" line="132"/>
+        <source>Unit step function</source>
+        <translation>Einheitssprung</translation>
+    </message>
+    <message>
+        <location filename="../models/functionModel.py" line="153"/>
+        <source>Sine function</source>
+        <translation>Sinusfunktion</translation>
+    </message>
+    <message>
+        <location filename="../models/functionModel.py" line="185"/>
+        <source>Cosine function</source>
+        <translation>Kosinusfunktion</translation>
+    </message>
+    <message>
+        <location filename="../views/functionView.py" line="72"/>
+        <source>Time [s]</source>
+        <translation>Zeit [s]</translation>
+    </message>
+    <message>
+        <location filename="../views/functionView.py" line="73"/>
+        <source>Output</source>
+        <translation>Ausgang</translation>
+    </message>
+</context>
+<context>
+    <name>FunctionView</name>
+    <message>
+        <location filename="../views/functionView.py" line="172"/>
+        <source>Excitation Function</source>
+        <translation>Anregungsfunktion</translation>
+    </message>
+</context>
+<context>
+    <name>PlantView</name>
+    <message>
+        <location filename="../views/plantView.py" line="67"/>
+        <location filename="../views/plantView.py" line="166"/>
+        <source>plant.num</source>
+        <translation>Nenner</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="81"/>
+        <location filename="../views/plantView.py" line="167"/>
+        <source>plant.den</source>
+        <translation>Zähler</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="165"/>
+        <source>Plant</source>
+        <translation>Regelstrecke</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="168"/>
+        <source>e.g. 1  → 1</source>
+        <translation>z.B. 1  → 1</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="169"/>
+        <source>e.g. 1, 0, 0  → 1*s^2 + 0*s + 0</source>
+        <translation>z.B. 1, 0, 0  → 1*s² + 0*s + 0</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="171"/>
+        <source>tooltip_num_den</source>
+        <translation>Verwenden Sie „.“ als Dezimaltrennzeichen.
+Die erste Zahl entspricht der höchsten Potenz von s.
+Beispiel: 1, 0.5, 2 → 1·s² + 0,5·s + 2</translation>
+    </message>
+    <message>
+        <source>Enter coefficients separated by commas, spaces, or semicolons.
+Use &apos;.&apos; as the decimal point.
+The first number corresponds to the highest power of s.
+Example: 1, 0.5, 2 → 1*s^2 + 0.5*s + 2</source>
+        <translation type="vanished">Verwenden Sie „.“ als Dezimaltrennzeichen.
+Die erste Zahl entspricht der höchsten Potenz von s.
+Beispiel: 1, 0.5, 2 → 1·s² + 0,5·s + 2</translation>
+    </message>
+</context>
+<context>
+    <name>PlotView</name>
+    <message>
+        <location filename="../views/plotView.py" line="116"/>
+        <source>plot.grid</source>
+        <translation>Grid</translation>
+    </message>
+    <message>
+        <location filename="../views/plotView.py" line="117"/>
+        <source>plot.start</source>
+        <translation>Startzeit</translation>
+    </message>
+    <message>
+        <location filename="../views/plotView.py" line="118"/>
+        <source>plot.end</source>
+        <translation>Endzeit</translation>
+    </message>
+    <message>
+        <location filename="../views/plotView.py" line="119"/>
+        <source>plot.start.tooltip</source>
+        <translation>Untere Grenze der x-Achse (x_min).
+Legt fest, wo die Zeitachse beginnt.
+Einheit: Sekunden (s).</translation>
+    </message>
+    <message>
+        <location filename="../views/plotView.py" line="120"/>
+        <source>plot.end.tooltip</source>
+        <translation>Obere Grenze der x-Achse (x_max).
+Legt fest, wo die Zeitachse endet.
+Einheit: Sekunden (s).
+Muss grösser als die Startzeit sein.</translation>
+    </message>
+</context>
+<context>
+    <name>function.view</name>
+    <message>
+        <source>Excitation Function</source>
+        <translation type="vanished">Anregungsfunktion</translation>
+    </message>
+    <message>
+        <source>Time [s]</source>
+        <translation type="vanished">Zeit [s]</translation>
+    </message>
+    <message>
+        <source>Output</source>
+        <translation type="vanished">Ausgang</translation>
+    </message>
+</context>
+<context>
+    <name>plant.view</name>
+    <message>
+        <location filename="../views/plantView.py" line="128"/>
+        <source>Step Response</source>
+        <translation>Sprungantwort</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="129"/>
+        <source>Time [s]</source>
+        <translation>Zeit [s]</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="130"/>
+        <source>Output</source>
+        <translation>Ausgang</translation>
+    </message>
+</context>
 </TS>
--- src/i18n/app_en.ts
+++ src/i18n/app_en.ts
@@ -1,4 +1,154 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
+<context>
+    <name>FunctionModel</name>
+    <message>
+        <location filename="../models/functionModel.py" line="132"/>
+        <source>Unit step function</source>
+        <translation>Unit step function</translation>
+    </message>
+    <message>
+        <location filename="../models/functionModel.py" line="153"/>
+        <source>Sine function</source>
+        <translation>Sine function</translation>
+    </message>
+    <message>
+        <location filename="../models/functionModel.py" line="185"/>
+        <source>Cosine function</source>
+        <translation>Cosine function</translation>
+    </message>
+    <message>
+        <location filename="../views/functionView.py" line="72"/>
+        <source>Time [s]</source>
+        <translation>Time [s]</translation>
+    </message>
+    <message>
+        <location filename="../views/functionView.py" line="73"/>
+        <source>Output</source>
+        <translation>Output</translation>
+    </message>
+</context>
+<context>
+    <name>FunctionView</name>
+    <message>
+        <location filename="../views/functionView.py" line="172"/>
+        <source>Excitation Function</source>
+        <translation>Excitation Function</translation>
+    </message>
+</context>
+<context>
+    <name>PlantView</name>
+    <message>
+        <location filename="../views/plantView.py" line="67"/>
+        <location filename="../views/plantView.py" line="166"/>
+        <source>plant.num</source>
+        <translation>Numerator</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="81"/>
+        <location filename="../views/plantView.py" line="167"/>
+        <source>plant.den</source>
+        <translation>Denumerator</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="165"/>
+        <source>Plant</source>
+        <translation>Plant</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="168"/>
+        <source>e.g. 1  → 1</source>
+        <translation>e.g. 1  → 1</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="169"/>
+        <source>e.g. 1, 0, 0  → 1*s^2 + 0*s + 0</source>
+        <translation>e.g. 1, 0, 0  → 1*s² + 0*s + 0</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="171"/>
+        <source>tooltip_num_den</source>
+        <translation>Enter coefficients separated by commas, spaces, or semicolons.
+Use &apos;.&apos; as the decimal point.
+The first number corresponds to the highest power of s.
+Example: 1, 0.5, 2 → 1*s² + 0.5*s + 2</translation>
+    </message>
+    <message>
+        <source>Enter coefficients separated by commas, spaces, or semicolons.
+Use &apos;.&apos; as the decimal point.
+The first number corresponds to the highest power of s.
+Example: 1, 0.5, 2 → 1*s^2 + 0.5*s + 2</source>
+        <translation type="vanished">Enter coefficients separated by commas, spaces, or semicolons.
+Use &apos;.&apos; as the decimal point.
+The first number corresponds to the highest power of s.
+Example: 1, 0.5, 2 → 1*s^2 + 0.5*s + 2</translation>
+    </message>
+</context>
+<context>
+    <name>PlotView</name>
+    <message>
+        <location filename="../views/plotView.py" line="116"/>
+        <source>plot.grid</source>
+        <translation>Grid</translation>
+    </message>
+    <message>
+        <location filename="../views/plotView.py" line="117"/>
+        <source>plot.start</source>
+        <translation>Start time</translation>
+    </message>
+    <message>
+        <location filename="../views/plotView.py" line="118"/>
+        <source>plot.end</source>
+        <translation>End time</translation>
+    </message>
+    <message>
+        <location filename="../views/plotView.py" line="119"/>
+        <source>plot.start.tooltip</source>
+        <translation>Lower x-axis limit (x_min).
+Defines where the time axis begins.
+Unit: seconds (s).</translation>
+    </message>
+    <message>
+        <location filename="../views/plotView.py" line="120"/>
+        <source>plot.end.tooltip</source>
+        <translation>Upper x-axis limit (x_max).
+Defines where the time axis ends.
+Unit: seconds (s).
+Must be greater than the start time.</translation>
+    </message>
+</context>
+<context>
+    <name>function.view</name>
+    <message>
+        <source>Excitation Function</source>
+        <translation type="vanished">Excitation Function</translation>
+    </message>
+    <message>
+        <source>Time [s]</source>
+        <translation type="vanished">Time [s]</translation>
+    </message>
+    <message>
+        <source>Output</source>
+        <translation type="vanished">Output</translation>
+    </message>
+</context>
+<context>
+    <name>plant.view</name>
+    <message>
+        <location filename="../views/plantView.py" line="128"/>
+        <source>Step Response</source>
+        <translation>Step Response</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="129"/>
+        <source>Time [s]</source>
+        <translation>Time [s]</translation>
+    </message>
+    <message>
+        <location filename="../views/plantView.py" line="130"/>
+        <source>Output</source>
+        <translation>Output</translation>
+    </message>
+</context>
 </TS>
