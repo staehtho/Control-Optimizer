8914e85fb6670afc0e88f753f42057a042756b13
Thomas Stähli <staehtho@students.zhaw.ch>
2026-03-03 07:37:18 +0100

    updated TODOs

--- PlayGround/mvvm_test.py
+++ PlayGround/mvvm_test.py
@@ -1,8 +1,6 @@
 from app_domain.controlsys import ExcitationTarget
 
 # TODO: Evaluation tab erst nach erstem PSO
-# TODO: start und endzeit von evaluation im plot verbinden
-# TODO: legende von plot ausserhalb
 # TODO: Stellgrösse als subplot -> PlotWidget subplot
 # TODO: BodePlot von PlotWidget erben oder ein BasePlotWidget?
 # TODO: SettingsView
