92284b929a7eba749b9fa29efe431155f52344de
Thomas Stähli <staehtho@students.zhaw.ch>
2026-03-04 20:06:36 +0100

    add set_font_size

--- src/views/widgets/formula_widget.py
+++ src/views/widgets/formula_widget.py
@@ -5,16 +5,27 @@ from utils import LatexRenderer
 
 
 class FormulaWidget(QLabel):
-    def __init__(self, formula: str, font_size_scale: float = 1, parent: QWidget = None):
+    def __init__(self, formula: str = "", font_size_scale: float = 1, parent: QWidget = None):
         super().__init__(parent)
 
+        self._formula = formula
         self._font_size_scale = font_size_scale
 
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.setStyleSheet("background: transparent;")
 
-        self.setPixmap(LatexRenderer.latex2pixmap(formula, font_size_scale=self._font_size_scale))
-        self.setAlignment(Qt.AlignmentFlag.AlignVCenter)
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self._update_formula()
 
     def set_formula(self, formula: str) -> None:
-        self.setPixmap(LatexRenderer.latex2pixmap(formula, font_size_scale=self._font_size_scale))
+        self._formula = formula
+        self._update_formula()
+
+    def set_font_size(self, font_size: float) -> None:
+        self._font_size_scale = font_size
+        self._update_formula()
+
+    def _update_formula(self) -> None:
+        if self._formula != "":
+            self.setPixmap(LatexRenderer.latex2pixmap(self._formula, font_size_scale=self._font_size_scale))
