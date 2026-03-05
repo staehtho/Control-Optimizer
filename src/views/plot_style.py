aaa8972268abf3fa4cdedbd2699f01e45e736ae2
Thomas Stähli <staehtho@students.zhaw.ch>
2026-03-05 19:55:33 +0100

    reordered Plot order

--- src/views/plot_style.py
+++ src/views/plot_style.py
@@ -7,6 +7,7 @@ from views.translations import PlotLabels
 @dataclass(frozen=True)
 class PlotStyle:
     color: str
+    plot_order: int
     linestyle: str = "-"
     marker: str = ""
     markersize: int = 0
@@ -22,11 +23,11 @@ class PlotStyle:
 
 
 PLOT_STYLE: Mapping[PlotLabels, PlotStyle] = {
-    PlotLabels.REFERENCE: PlotStyle("#ff7f0e", z_order=11),
-    PlotLabels.INPUT_DISTURBANCE: PlotStyle("#2ca02c", z_order=12),
-    PlotLabels.MEASUREMENT_DISTURBANCE: PlotStyle("#d62728", z_order=13),
-    PlotLabels.PLANT: PlotStyle("#7f7f7f", z_order=14),
-    PlotLabels.FUNCTION: PlotStyle("#1f77b4"),
-    PlotLabels.CLOSED_LOOP: PlotStyle("#1f77b4", z_order=15),
-    PlotLabels.CONTROL_SIGNAL: PlotStyle("#9467bd", z_order=10),
+    PlotLabels.PLANT: PlotStyle("#7f7f7f", 1, z_order=14),
+    PlotLabels.CLOSED_LOOP: PlotStyle("#1f77b4", 2, z_order=15),
+    PlotLabels.REFERENCE: PlotStyle("#ff7f0e", 3, z_order=11),
+    PlotLabels.INPUT_DISTURBANCE: PlotStyle("#2ca02c", 4, z_order=12),
+    PlotLabels.MEASUREMENT_DISTURBANCE: PlotStyle("#d62728", 5, z_order=13),
+    PlotLabels.CONTROL_SIGNAL: PlotStyle("#9467bd", 6, z_order=10),
+    PlotLabels.FUNCTION: PlotStyle("#1f77b4", 7),
 }
