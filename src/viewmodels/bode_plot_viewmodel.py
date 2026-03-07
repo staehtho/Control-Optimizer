d60d9bb79da3f8c0acd8ff4daa332107427a54ca
Thomas Stähli <staehtho@students.zhaw.ch>
2026-03-07 18:52:54 +0100

    new BodePlotViewModel

--- src/viewmodels/bode_plot_viewmodel.py
+++ src/viewmodels/bode_plot_viewmodel.py
@@ -1,3 +1,4 @@
+from utils import LoggedProperty
 from .plot_viewmodel import PlotViewModel
 
 
@@ -8,13 +9,22 @@ class BodePlotViewModel(PlotViewModel):
         self._x_min: float = 1e-2
         self._x_max: float = 1e5
 
+    # -------------------
+    # x min
+    # -------------------
     def _verify_x_min(self, value: float) -> bool:
         if value >= self._x_max:
             self.logger.warning(f"Attempted to set x_min >= x_max ({value} >= {self._x_max})")
             return False
-
-        if value == 0:
+        if value == 0.0:
             self.logger.warning(f"Attempted to set x_min 0 ({value} >= 0)")
             return False
 
         return True
+
+    x_min = LoggedProperty(
+        path="_x_min",
+        signal="xMinChanged",
+        typ=float,
+        custom_setter=_verify_x_min,
+    )
