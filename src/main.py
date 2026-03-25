0dda6ee7ec93487c77e663931441ed36665581ab
Thomas Stähli <staehtho@students.zhaw.ch>
2026-03-25 17:35:37 +0100

    reduce startup time

--- src/main.py
+++ src/main.py
@@ -2,29 +2,36 @@ import logging
 import sys
 from pathlib import Path
 
+from time import time
+
 from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QApplication
 
 # TODO: add buttons to switch to the next or previous view in the view it self
 # TODO: TabIndex
+# TODO: layout switches in off state
 
 # TODO: BodePlot: when all plots are disabled, the y-axis should not be rescaled. now it scale to 99999 or so :)
 
 # TODO: Controller: move the block diagram and the selection of the anti windup in one section called anti windup
 #  and call the selection method or so
+# TODO: Controller: block diagram clamping, remove the saturation block and add in the integrator the range
 
 # TODO: PSO: overshoot only for step
+# TODO: PSO: set default stability to 6 dB
+# TODO: PSO: add stellrantenbegrenzung
 
 # TODO: Evaluation: block diagram add l and n input
 # TODO: Evaluation: TF with L and N
 # TODO: Evaluation: clean TF of C, G, etc.
 
 def main():
+    start_time = time()
+    print(f"start: {time() - start_time:.3f}s")
     from app_domain import AppEngine
-    from app_domain.functions import FunctionTypes
-    from app_types import NavItem, NavLabels
-    from views.main_view import MainView
-    from views.resources import Icons, SRC_DIR
+    print(f"import AppEngine: {time() - start_time:.3f}s")
+    from views.resources import SRC_DIR
+    print(f"import resources: {time() - start_time:.3f}s")
 
     # init logging
     log_dir = Path(SRC_DIR) / "logs"
@@ -47,21 +54,14 @@ def main():
         logger = logging.getLogger(log)
         logger.setLevel(logging.CRITICAL + 1)
 
+    print(f"log: {time() - start_time:.3f}s")
+
     app = QApplication(sys.argv)
 
-    engine = AppEngine(run_warmup=False)
+    engine = AppEngine()
+    print(f"AppEngine: {time() - start_time:.3f}s")
     app.aboutToQuit.connect(engine.shutdown)
     ui_context = engine.ui_context
-
-    items = [
-        NavItem(NavLabels.PLANT, Icons.plant),
-        NavItem(NavLabels.EXCITATION_FUNCTION, Icons.excitation_function),
-        NavItem(NavLabels.CONTROLLER, Icons.controller),
-        NavItem(NavLabels.PSO_PARAMETER, Icons.pso_parameter),
-        NavItem(NavLabels.EVALUATION, Icons.evaluation),
-        NavItem(NavLabels.SIMULATION, Icons.simulation),
-        NavItem(NavLabels.SETTINGS, Icons.settings, bottom=True),
-    ]
 
     def _create_plant_view(parent=None):
         from views.plant_view import PlantView
@@ -74,6 +74,7 @@ def main():
 
     def _create_function_view(parent=None):
         from views.function_view import FunctionView
+        from app_domain.functions import FunctionTypes
         vm_function = engine.ensure_function_viewmodel("excitation_target")
         vm_function.set_selected_function(FunctionTypes.STEP)
         return FunctionView(
@@ -121,19 +122,36 @@ def main():
         from views.settings_view import SettingsView
         return SettingsView(ui_context, parent=parent)
 
-    view_factories = {
-        NavLabels.PLANT: _create_plant_view,
-        NavLabels.EXCITATION_FUNCTION: _create_function_view,
-        NavLabels.CONTROLLER: _create_controller_view,
-        NavLabels.PSO_PARAMETER: _create_pso_configuration_view,
-        NavLabels.EVALUATION: _create_evaluation_view,
-        NavLabels.SIMULATION: _create_simulation_view,
-        NavLabels.SETTINGS: _create_settings_view,
-    }
+    def _create_main_view():
+        from views.main_view import MainView
+        from app_types import NavItem, NavLabels
+        from views.resources import Icons
+        view_factories = {
+            NavLabels.PLANT: _create_plant_view,
+            NavLabels.EXCITATION_FUNCTION: _create_function_view,
+            NavLabels.CONTROLLER: _create_controller_view,
+            NavLabels.PSO_PARAMETER: _create_pso_configuration_view,
+            NavLabels.EVALUATION: _create_evaluation_view,
+            NavLabels.SIMULATION: _create_simulation_view,
+            NavLabels.SETTINGS: _create_settings_view,
+        }
 
-    main_view = MainView(ui_context, items, view_factories, engine.ensure_pso_viewmodel())
+        items = [
+            NavItem(NavLabels.PLANT, Icons.plant),
+            NavItem(NavLabels.EXCITATION_FUNCTION, Icons.excitation_function),
+            NavItem(NavLabels.CONTROLLER, Icons.controller),
+            NavItem(NavLabels.PSO_PARAMETER, Icons.pso_parameter),
+            NavItem(NavLabels.EVALUATION, Icons.evaluation),
+            NavItem(NavLabels.SIMULATION, Icons.simulation),
+            NavItem(NavLabels.SETTINGS, Icons.settings, bottom=True),
+        ]
+
+        return MainView(ui_context, items, view_factories, engine.ensure_pso_viewmodel())
+
+    print(f"init Main View: {time() - start_time:.3f}s")
+    main_view = _create_main_view()
     main_view.show()
-
+    print(f"MainView: {time() - start_time:.3f}s")
     QTimer.singleShot(750, lambda: engine.run_warmup(2))
 
     sys.exit(app.exec())
