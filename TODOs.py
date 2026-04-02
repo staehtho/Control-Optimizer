2882359cb4b27e418f9ca8e8008fbf5bea644926
Thomas Stähli <staehtho@students.zhaw.ch>
2026-04-02 10:03:07 +0200

    TODOs

--- TODOs.py
+++ TODOs.py
@@ -1,2 +1,7 @@
 # TODO: Verhalten, wenn keine feasible Lösung gefunden werden kann
 # TODO: Performance untersuchen / verbessern
+
+# TODO: PA mit BA verglichen -> nicht kaput gemacht :) (Anti-Windup)
+
+# TODO: evtl. im Bericht im theorieteil zeigen, wie totzeit in UTF approximiert werden kann, da wir nur UTF darstellen können
+#   (System identifikation, Totzeit in LTI überführen)
