7a21bb6acf89fbd67e8a43d548d43380c06a7bb7
flo <buechfl2@students.zhaw.ch>
2026-04-05 20:41:26 +1200

    Todo's

--- TODOs.py
+++ TODOs.py
@@ -1,9 +1,17 @@
-# TODO: Verhalten, wenn keine feasible Lösung gefunden werden kann
+
 # TODO: Performance untersuchen / verbessern
-
 # TODO: PA mit BA verglichen -> nicht kaput gemacht :) (Anti-Windup)
-
 # TODO: evtl. im Bericht im theorieteil zeigen, wie totzeit in UTF approximiert werden kann, da wir nur UTF darstellen können
 #   (System identifikation, Totzeit in LTI überführen)
+# TODO: Frequenzbereich in app fixiert? e-5 bis e5?
+# TODO: Auflösung im Frequenzbereich diskutieren
 
+
+# TODO: Verhalten, wenn keine feasible Lösung gefunden werden kann
 # TODO: evt hyperparameter von PSO in systemsettings integrieren?
+# TODO: Wo ist die automatische Simulationszeit in App
+# TODO: Minimalwert Ti auf 0.001?
+# TODO: schieberegler farbe in light mode
+# TODO: Plots in App zu gross
+# TODO: Einheit von slewrate du/dt in Evaluationsfenster. Evt auch Fenstergrösse anzeigen?
+
