d1d93061948fbf45390666ef1af8c16549ae9b37
Thomas Stähli <staehtho@students.zhaw.ch>
2026-04-09 08:29:10 +0200

    pso snapshot now contains the pso configuration

--- src/models/pso_simulation_snapshot.py
+++ src/models/pso_simulation_snapshot.py
@@ -9,6 +9,7 @@ class PsoSimulationSnapshot:
     plant_num: tuple[float, ...]
     plant_den: tuple[float, ...]
     plant_tf: str
+
     controller_type: str
     controller_anti_windup: AntiWindup
     controller_ka: float
@@ -16,6 +17,22 @@ class PsoSimulationSnapshot:
     controller_constraint_max: float
     controller_tuning_factor: float
     sampling_rate: float | None
+
+    simulation_time: tuple[float, float]
     excitation_target: ExcitationTarget
     excitation_function: BaseFunction
     error_criterion: PerformanceIndex
+    kp: tuple[float, float]
+    ti: tuple[float, float]
+    td: tuple[float, float]
+    overshoot_control: float
+    overshoot_control_enabled: bool
+    slew_rate_max: float
+    slew_window_size: int
+    slew_rate_limit_enabled: bool
+    gain_margin: float
+    gain_margin_enabled: bool
+    phase_margin: float
+    phase_margin_enabled: bool
+    stability_margin: float
+    stability_margin_enabled: bool
