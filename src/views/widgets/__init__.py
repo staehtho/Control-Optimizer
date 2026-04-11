297ce19869e7ea238810a9f9114444c12f7b8576
Thomas Stähli <staehtho@students.zhaw.ch>
2026-04-11 10:38:29 +0200

    add info banner

--- src/views/widgets/__init__.py
+++ src/views/widgets/__init__.py
@@ -4,6 +4,23 @@ from .toggleable_section_frame import ToggleableSectionFrame
 from .plot_widget import PlotWidget, PlotWidgetConfiguration, SubplotConfiguration
 from .bode_plot_widget import BodePlotWidget
 from .function_widget import FunctionWidget
+from .info_banner import InfoBanner
 from .navigation_widget import NavigationWidget
 from .formula_widget import FormulaWidget
 from .toggle_switch import ToggleSwitch, TextPosition
+
+__all__ = [
+    "AspectRatioSvgWidget",
+    "SectionFrame",
+    "ToggleableSectionFrame",
+    "PlotWidget",
+    "PlotWidgetConfiguration",
+    "SubplotConfiguration",
+    "BodePlotWidget",
+    "FunctionWidget",
+    "InfoBanner",
+    "NavigationWidget",
+    "FormulaWidget",
+    "ToggleSwitch",
+    "TextPosition",
+]
---
+++ src/views/widgets/info_banner.py
@@ -0,0 +1,47 @@
+from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout
+from PySide6.QtCore import QTimer, Qt, QPropertyAnimation, QEasingCurve
+from PySide6.QtGui import QColor, QPalette
+
+
+class InfoBanner(QWidget):
+    def __init__(self, text: str, parent: QWidget = None):
+        super().__init__(parent)
+
+        self.setAutoFillBackground(True)
+        pal = self.palette()
+        pal.setColor(QPalette.ColorRole.Window, QColor("#2d8cff"))  # blue banner
+        pal.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
+        self.setPalette(pal)
+
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(15, 10, 15, 10)
+
+        self.label = QLabel(text)
+        self.label.setStyleSheet("font-weight: bold;")
+        layout.addWidget(self.label)
+
+        self.setFixedHeight(40)
+        self.setVisible(False)
+
+        # fade animation
+        self._anim = QPropertyAnimation(self, b"windowOpacity")
+        self._anim.setDuration(400)
+        self._anim.setEasingCurve(QEasingCurve.Type.InOutQuad)
+
+    def show_banner(self, duration_ms: int = 2500):
+        self.setWindowOpacity(0.0)
+        self.setVisible(True)
+
+        self._anim.stop()
+        self._anim.setStartValue(0.0)
+        self._anim.setEndValue(1.0)
+        self._anim.start()
+
+        QTimer.singleShot(duration_ms, self.hide_banner)
+
+    def hide_banner(self):
+        self._anim.stop()
+        self._anim.setStartValue(1.0)
+        self._anim.setEndValue(0.0)
+        self._anim.start()
+        self._anim.finished.connect(lambda: self.setVisible(False))
