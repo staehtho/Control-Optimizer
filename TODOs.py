8c30d96ab3058d18e34703bce5dfdd35a488112e
Thomas Stähli <staehtho@students.zhaw.ch>
2026-05-09 09:56:55 +0200

    todos

--- TODOs.py
+++ TODOs.py
@@ -1,10 +1,5 @@
-
-# TODO: Performance untersuchen / verbessern
-# TODO: PA mit BA verglichen -> nicht kaput gemacht :) (Anti-Windup)
-# TODO: evtl. im Bericht im theorieteil zeigen, wie totzeit in UTF approximiert werden kann, da wir nur UTF darstellen können
-#   (System identifikation, Totzeit in LTI überführen)
-# TODO: Frequenzbereich in app fixiert? e-5 bis e5?
-# TODO: Auflösung im Frequenzbereich diskutieren
-
 # TODO: Meinung von Thomas: da der Code veröffentlicht wird, sollten am Ende alle nicht relevanten Skripte entweder in
 #   in das Doku-Repo verschoben oder gelöscht werden (PlayGround)
+
+# TODO: values 3 Signifikate stellen (Evaluation, Report)
+# TODO: BodePlot nicht start und Endtime !!!
