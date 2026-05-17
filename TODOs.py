b991b5f9bc0c0dad2e968764bf149c81d69f46f4
Thomas Stähli <staehtho@students.zhaw.ch>
2026-05-17 13:55:55 +0200

    fixed todo

--- TODOs.py
+++ TODOs.py
@@ -1,5 +1,2 @@
 # TODO: Meinung von Thomas: da der Code veröffentlicht wird, sollten am Ende alle nicht relevanten Skripte entweder in
 #   in das Doku-Repo verschoben oder gelöscht werden (PlayGround)
-
-# TODO: Zeitfenster verbessern in Plot -> nehme dt -> mit dt nimmt die Performance ab -> es dauert um einiges länger für die visualisierung -> viel mehr Daten
-#   nehme dennoch dt -> Sanduhr für symbolisierung es wird gerechnet
